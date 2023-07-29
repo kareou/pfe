7b8ebf9a6da469af2a665d7e1bf01f33e9e69ff7
mohamed khairoun <xxmesxx2@gmail.com>
2023-07-30 00:23:41 +0100

    loging

--- resources/js/Pages/Auth/Login.jsx
+++ resources/js/Pages/Auth/Login.jsx
@@ -1,97 +1,148 @@
-import { useEffect } from 'react';
-import Checkbox from '@/Components/Checkbox';
-import GuestLayout from '@/Layouts/GuestLayout';
-import InputError from '@/Components/InputError';
-import InputLabel from '@/Components/InputLabel';
-import PrimaryButton from '@/Components/PrimaryButton';
-import TextInput from '@/Components/TextInput';
-import { Head, Link, useForm } from '@inertiajs/react';
+import { useEffect } from "react";
+import Checkbox from "@/Components/Checkbox";
+import GuestLayout from "@/Layouts/GuestLayout";
+import InputError from "@/Components/InputError";
+import InputLabel from "@/Components/InputLabel";
+import PrimaryButton from "@/Components/PrimaryButton";
+import TextInput from "@/Components/TextInput";
+import { Head, Link, useForm } from "@inertiajs/react";
+import { FcGoogle } from "react-icons/fc";
+import vid from "../../../../../../Downloads/pexels-tima-miroshnichenko-7989632 (2160p).mp4";
 
 export default function Login({ status, canResetPassword }) {
     const { data, setData, post, processing, errors, reset } = useForm({
-        email: '',
-        password: '',
+        email: "",
+        password: "",
         remember: false,
     });
 
     useEffect(() => {
         return () => {
-            reset('password');
+            reset("password");
         };
     }, []);
 
     const submit = (e) => {
         e.preventDefault();
 
-        post(route('login'));
+        post(route("login"));
     };
 
     return (
         <GuestLayout>
             <Head title="Log in" />
 
-            {status && <div className="mb-4 font-medium text-sm text-green-600">{status}</div>}
+            {status && (
+                <div className="mb-4 font-medium text-sm text-green-600">
+                    {status}
+                </div>
+            )}
+            <div className=" w-[347px] h-full flex-grow-0">
+                <video
+                    autoPlay
+                    loop
+                    muted
+                    className=" object-cover block" // Set the desired height (e.g., h-300)
+                    src={vid}
+                ></video>
+            </div>
+            <div className="p-8 ml-24 grid gap-1 ">
+                <h1 className=" text-2xl text-my_gray2 font-bold">
+                    Sign in to sitednm
+                </h1>
 
-            <form onSubmit={submit}>
-                <div>
-                    <InputLabel htmlFor="email" value="Email" />
+                <button className="flex items-center border border-my_gray/60 justify-center mt-4 w-96 h-12 bg-my_blue text-my_gray2 rounded-md">
+                    <FcGoogle className="w-6 h-6 mr-2" />
+                    <span className="text-sm font-semibold">
+                        Sign in with Google
+                    </span>
+                </button>
 
-                    <TextInput
-                        id="email"
-                        type="email"
-                        name="email"
-                        value={data.email}
-                        className="mt-1 block w-full"
-                        autoComplete="username"
-                        isFocused={true}
-                        onChange={(e) => setData('email', e.target.value)}
-                    />
-
-                    <InputError message={errors.email} className="mt-2" />
+                <div className="flex items-center my-4">
+                    <div className="flex-grow border-t border-my_gray/60"></div>
+                    <span className="mx-4 text-my_gray2 font-semibold">
+                        or Sign with Email
+                    </span>
+                    <div className="flex-grow border-t border-my_gray/60"></div>
                 </div>
 
-                <div className="mt-4">
-                    <InputLabel htmlFor="password" value="Password" />
+                <form onSubmit={submit} className="grid gap-2">
+                    <div>
+                        <InputLabel htmlFor="email" value="Email" />
 
-                    <TextInput
-                        id="password"
-                        type="password"
-                        name="password"
-                        value={data.password}
-                        className="mt-1 block w-full"
-                        autoComplete="current-password"
-                        onChange={(e) => setData('password', e.target.value)}
-                    />
-
-                    <InputError message={errors.password} className="mt-2" />
-                </div>
-
-                <div className="block mt-4">
-                    <label className="flex items-center">
-                        <Checkbox
-                            name="remember"
-                            checked={data.remember}
-                            onChange={(e) => setData('remember', e.target.checked)}
+                        <TextInput
+                            id="email"
+                            type="email"
+                            name="email"
+                            value={data.email}
+                            className="  mt-1 block w-96 focus:ring-my_red/50 focus:ring-1 focus:border-none focus:shadow-my_red/75 focus:shadow-lg sm:text-sm border-gray-300 rounded-md"
+                            autoComplete="username"
+                            isFocused={true}
+                            onChange={(e) => setData("email", e.target.value)}
                         />
-                        <span className="ml-2 text-sm text-gray-600">Remember me</span>
-                    </label>
-                </div>
 
-                <div className="flex items-center justify-end mt-4">
-                    {canResetPassword && (
-                        <Link
-                            href={route('password.request')}
-                            className="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                        >
-                            Forgot your password?
-                        </Link>
-                    )}
+                        <InputError message={errors.email} className="mt-2" />
+                    </div>
 
-                    <PrimaryButton className="ml-4" disabled={processing}>
+                    <div className="mt-4 relative">
+                        <InputLabel htmlFor="password" value="Password" />
+
+                        <TextInput
+                            id="password"
+                            type="password"
+                            name="password"
+                            value={data.password}
+                            className="  mt-1 block w-96 focus:ring-my_red/50 focus:ring-1 focus:border-none focus:shadow-my_red/75 focus:shadow-lg sm:text-sm border-gray-300 rounded-md"
+                            autoComplete="current-password"
+                            onChange={(e) =>
+                                setData("password", e.target.value)
+                            }
+                        />
+
+                        <InputError
+                            message={errors.password}
+                            className="mt-2"
+                        />
+                        {canResetPassword && (
+                            <Link
+                                href={route("password.request")}
+                                className="absolute underline top-0 right-0 text-sm text-my_gray2 hover:text-my_red/50"
+                            >
+                                Forgot?
+                            </Link>
+                        )}
+                    </div>
+
+                    <div className="block mt-4">
+                        <label className="flex items-center">
+                            <Checkbox
+                                name="remember"
+                                checked={data.remember}
+                                onChange={(e) =>
+                                    setData("remember", e.target.checked)
+                                }
+                            />
+                            <span className="ml-2 text-sm text-gray-600">
+                                Remember me
+                            </span>
+                        </label>
+                    </div>
+
+                    <PrimaryButton
+                        className=" flex justify-center  mt-4 w-96  sm:text-sm border-gray-300 rounded-md"
+                        disabled={processing}
+                    >
                         Log in
                     </PrimaryButton>
-                </div>
-            </form>
+                </form>
+                <Link
+                    className="flex justify-center text-sm text-my_gray2 hover:text-my_red/50"
+                    href={route("register")}
+                >
+                    <span className="">Don't have an account?</span>
+                    <span className="underline">Sign Up</span>
+                </Link>
+            </div>
         </GuestLayout>
     );
 }
--- resources/js/Pages/Auth/Register.jsx
+++ resources/js/Pages/Auth/Register.jsx
@@ -1,117 +1,199 @@
-import { useEffect } from 'react';
-import GuestLayout from '@/Layouts/GuestLayout';
-import InputError from '@/Components/InputError';
-import InputLabel from '@/Components/InputLabel';
-import PrimaryButton from '@/Components/PrimaryButton';
-import TextInput from '@/Components/TextInput';
-import { Head, Link, useForm } from '@inertiajs/react';
+import { useEffect, useState } from "react";
+import GuestLayout from "@/Layouts/GuestLayout";
+import InputError from "@/Components/InputError";
+import InputLabel from "@/Components/InputLabel";
+import PrimaryButton from "@/Components/PrimaryButton";
+import TextInput from "@/Components/TextInput";
+import { Head, Link, useForm } from "@inertiajs/react";
+import vid from "../../../../../../Downloads/pexels-tima-miroshnichenko-7989632 (2160p).mp4";
+import { FcGoogle } from "react-icons/fc";
+import { IoIosArrowBack } from "react-icons/io";
 
 export default function Register() {
     const { data, setData, post, processing, errors, reset } = useForm({
-        name: '',
-        email: '',
-        password: '',
-        password_confirmation: '',
+        name: "",
+        email: "",
+        password: "",
+        password_confirmation: "",
     });
+
+    const [show, setShow] = useState(false);
 
     useEffect(() => {
         return () => {
-            reset('password', 'password_confirmation');
+            reset("password", "password_confirmation");
         };
     }, []);
 
     const submit = (e) => {
         e.preventDefault();
 
-        post(route('register'));
+        post(route("register"));
     };
 
     return (
         <GuestLayout>
             <Head title="Register" />
 
-            <form onSubmit={submit}>
-                <div>
-                    <InputLabel htmlFor="name" value="Name" />
+            <div className=" w-[347px] h-full flex-grow-0">
+                <video
+                    autoPlay
+                    loop
+                    muted
+                    className=" object-cover block" // Set the desired height (e.g., h-300)
+                    src={vid}
+                ></video>
+            </div>
 
-                    <TextInput
-                        id="name"
-                        name="name"
-                        value={data.name}
-                        className="mt-1 block w-full"
-                        autoComplete="name"
-                        isFocused={true}
-                        onChange={(e) => setData('name', e.target.value)}
-                        required
-                    />
-
-                    <InputError message={errors.name} className="mt-2" />
-                </div>
-
-                <div className="mt-4">
-                    <InputLabel htmlFor="email" value="Email" />
-
-                    <TextInput
-                        id="email"
-                        type="email"
-                        name="email"
-                        value={data.email}
-                        className="mt-1 block w-full"
-                        autoComplete="username"
-                        onChange={(e) => setData('email', e.target.value)}
-                        required
-                    />
-
-                    <InputError message={errors.email} className="mt-2" />
-                </div>
-
-                <div className="mt-4">
-                    <InputLabel htmlFor="password" value="Password" />
-
-                    <TextInput
-                        id="password"
-                        type="password"
-                        name="password"
-                        value={data.password}
-                        className="mt-1 block w-full"
-                        autoComplete="new-password"
-                        onChange={(e) => setData('password', e.target.value)}
-                        required
-                    />
-
-                    <InputError message={errors.password} className="mt-2" />
-                </div>
-
-                <div className="mt-4">
-                    <InputLabel htmlFor="password_confirmation" value="Confirm Password" />
-
-                    <TextInput
-                        id="password_confirmation"
-                        type="password"
-                        name="password_confirmation"
-                        value={data.password_confirmation}
-                        className="mt-1 block w-full"
-                        autoComplete="new-password"
-                        onChange={(e) => setData('password_confirmation', e.target.value)}
-                        required
-                    />
-
-                    <InputError message={errors.password_confirmation} className="mt-2" />
-                </div>
-
-                <div className="flex items-center justify-end mt-4">
-                    <Link
-                        href={route('login')}
-                        className="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+            {!show && (
+                <div className="p-8 ml-24 grid gap-1 ">
+                    <h1 className=" text-2xl text-my_gray2 font-bold">
+                        Sign up to Sitednm
+                    </h1>
+                    <button className="flex items-center border border-my_gray/60 justify-center mt-4 w-96 h-12 bg-my_blue text-my_gray2 rounded-md">
+                        <FcGoogle className="w-6 h-6 mr-2" />
+                        <span className="text-sm font-semibold">
+                            Sign up with Google
+                        </span>
+                    </button>
+                    <div className="flex items-center my-4">
+                        <div className="flex-grow border-t border-my_gray/60"></div>
+                        <div className="mx-4 text-lg text-my_gray2">Or</div>
+                        <div className="flex-grow border-t border-my_gray/60"></div>
+                    </div>
+                    <button
+                        className="flex items-center border bg-my_gray border-my_gray/60 justify-center mt-4 w-96 h-12 bg-my_blue text-my_white rounded-md"
+                        onClick={() => setShow(!show)}
                     >
-                        Already registered?
-                    </Link>
-
-                    <PrimaryButton className="ml-4" disabled={processing}>
-                        Register
-                    </PrimaryButton>
+                        <span className="text-sm font-semibold">
+                            Sign up with Email
+                        </span>
+                    </button>
                 </div>
-            </form>
+            )}
+            {show && (
+                <div className="p-8 ml-24 grid gap-1 relative register">
+                    <h1 className=" text-2xl text-my_gray2 font-extrabold mb-5">
+                        Sign up to Sitednm
+                    </h1>
+                    <button className=" -top-0 -left-5 absolute border border-my_gray w-10 h-10 rounded flex justify-center place-items-center"
+                        onClick={() => setShow(!show)}
+                    >
+                        <IoIosArrowBack className="w-6 h-6 mr-2" />
+                    </button>
+                    <form onSubmit={submit}>
+                        <div>
+                            <InputLabel htmlFor="name" value="Name" />
+
+                            <TextInput
+                                id="name"
+                                name="name"
+                                value={data.name}
+                                className="  mt-1 block w-96 focus:ring-my_red/50 focus:ring-1 focus:border-none focus:shadow-my_red/75 focus:shadow-lg sm:text-sm border-gray-300 rounded-md"
+                                autoComplete="name"
+                                isFocused={true}
+                                onChange={(e) =>
+                                    setData("name", e.target.value)
+                                }
+                                required
+                            />
+
+                            <InputError
+                                message={errors.name}
+                                className="mt-2"
+                            />
+                        </div>
+
+                        <div className="mt-4">
+                            <InputLabel htmlFor="email" value="Email" />
+
+                            <TextInput
+                                id="email"
+                                type="email"
+                                name="email"
+                                value={data.email}
+                                className="  mt-1 block w-96 focus:ring-my_red/50 focus:ring-1 focus:border-none focus:shadow-my_red/75 focus:shadow-lg sm:text-sm border-gray-300 rounded-md"
+                                autoComplete="username"
+                                onChange={(e) =>
+                                    setData("email", e.target.value)
+                                }
+                                required
+                            />
+
+                            <InputError
+                                message={errors.email}
+                                className="mt-2"
+                            />
+                        </div>
+
+                        <div className="mt-4">
+                            <InputLabel htmlFor="password" value="Password" />
+
+                            <TextInput
+                                id="password"
+                                type="password"
+                                name="password"
+                                value={data.password}
+                                className="  mt-1 block w-96 focus:ring-my_red/50 focus:ring-1 focus:border-none focus:shadow-my_red/75 focus:shadow-lg sm:text-sm border-gray-300 rounded-md"
+                                autoComplete="new-password"
+                                onChange={(e) =>
+                                    setData("password", e.target.value)
+                                }
+                                required
+                            />
+
+                            <InputError
+                                message={errors.password}
+                                className="mt-2"
+                            />
+                        </div>
+
+                        <div className="mt-4">
+                            <InputLabel
+                                htmlFor="password_confirmation"
+                                value="Confirm Password"
+                            />
+
+                            <TextInput
+                                id="password_confirmation"
+                                type="password"
+                                name="password_confirmation"
+                                value={data.password_confirmation}
+                                className="  mt-1 block w-96 focus:ring-my_red/50 focus:ring-1 focus:border-none focus:shadow-my_red/75 focus:shadow-lg sm:text-sm border-gray-300 rounded-md"
+                                autoComplete="new-password"
+                                onChange={(e) =>
+                                    setData(
+                                        "password_confirmation",
+                                        e.target.value
+                                    )
+                                }
+                                required
+                            />
+
+                            <InputError
+                                message={errors.password_confirmation}
+                                className="mt-2"
+                            />
+                        </div>
+
+                        <div className="grid grid-row-2 gap-4 mt-4">
+                            <PrimaryButton
+                                className=" flex justify-center  mt-4 w-96  sm:text-sm border-gray-300 rounded-md"
+                                disabled={processing}
+                            >
+                                Create Account
+                            </PrimaryButton>
+                            <Link
+                                className="flex justify-center text-sm text-my_gray2 hover:text-my_red/50"
+                                href={route("login")}
+                            >
+                                <span className="">Don't have an account?</span>
+                                <span className="underline">Sign Up</span>
+                            </Link>
+                        </div>
+                    </form>
+                </div>
+            )}
         </GuestLayout>
     );
 }
